442d63ab68f8943d9afa6ea3f7e01c274dffdec2
AndresD <felipediazc08@gmail.com>
2026-03-12 13:47:35 -0500

    Lanzamiento de dados

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Random random = new Random();
+        int d1,d2;
+        int sumad1=0,sumad2=0;
+        System.out.println("|== Bienvenido ==|");
+        System.out.println(" ");
+do{
+    d1=random.nextInt(6) + 1;
+    d2=random.nextInt(6) + 1;
+    sumad1+=d1;
+    sumad2+=d2;
+    System.out.println("Dado 1: " + d1 + " | Dado 2: " + d2);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+}while (d1 != d2);
+        System.out.println(" ");
+if(sumad1>sumad2){
+    System.out.println("|--- El dado ganador es el dado1: "+sumad1+" ---|");
+        } else if (sumad2>sumad1) {
+    System.out.println("|--- El dado ganador es el dado2: "+sumad2+" ---|");
+
+}else{
+    System.out.println("|--- Empate ---|");
+}
+        System.out.println(" ");
+        System.out.println("|== Programa finalizado ==|");
+
     }
 }
